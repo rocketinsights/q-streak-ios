a470b72c98fa7afbba783075f045a4cfa8e4b3d8
Clifford Anderson <cliff@rocketinsights.com>
2020-04-20 15:40:06 -0400

    implement navigation from Get Started button programatically rather than via storyboard; build out Welcome screen labels using Stack View and specify layout constraints

---
+++ QStreak/View Controllers/Account Setup/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  QStreak
+//
+//  Created by Clifford Anderson on 4/17/20.
+//  Copyright © 2020 Rocket Insights. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet private weak var getStartedButton: UIButton!
+
+    // MARK: - IBAction
+
+    @IBAction func getStartedButtonTapped(_ sender: Any) {
+        guard let accountSetupViewController = self.storyboard?.instantiateViewController(withIdentifier: String(describing: AccountSetupViewController.self)) else { return }
+        self.navigationController?.pushViewController(accountSetupViewController, animated: true)
+    }
+
+}
